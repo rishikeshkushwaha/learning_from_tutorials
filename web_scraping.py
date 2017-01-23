5a76caad0777770965924f3c14fea2181d91ae96
Rishikesh Kushwaha <rishikeshkushwaha@gmail.com>
2017-01-23 18:16:58 +0530

    web scarping tutorial from scraping tweets

---
+++ web_scraping.py
@@ -0,0 +1,31 @@
+import urllib2
+from urllib2 import Request
+from bs4 import BeautifulSoup
+
+
+#A small tutorial to learn beautifulsoup to scap tweets from any user based
+# on profile link
+
+theurl = Request("https://twitter.com/rishikeshkush")
+thepage = urllib2.urlopen(theurl)
+soup = BeautifulSoup(thepage,"html.parser")
+
+print(soup.title)
+#print soup.title.text
+#print(soup.findAll('a'))
+
+"""
+for link in soup.findAll('a'):
+    print link.get('href')
+    print link.text
+"""
+
+print soup.findAll('a')[0]
+#if soup.find('div',{"class":"ProfileHeaderClass"}).find('p') is not None:
+print soup.find('div', {"class":"ProfileHeaderCard"}).find('p').text
+
+i=1
+print soup.title.text
+for tweets in soup.findAll('div', {"class":"content"}):
+    print i,": ",tweets.find('p').text
+    i=i+1
