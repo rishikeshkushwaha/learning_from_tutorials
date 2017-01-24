b13bd7580a550c6ea640c182d747f1e4c23ec22c
Rishikesh Kushwaha <rishikeshkushwaha@gmail.com>
2017-01-24 13:37:53 +0530

    Web scraping code completed tutorial 2 and also .csv

--- web_scraping_table.py
+++ web_scraping_table.py
@@ -1,9 +1,10 @@
 import urllib2
 from urllib2 import Request
 from bs4 import BeautifulSoup
-
+import os
 
 #A small tutorial to read a table and then scarpe data into table format
+#and create .csv file to save it also.
 
 def make_soup(url):
     theurl = Request(url)
@@ -11,13 +12,22 @@ def make_soup(url):
     soupdata = BeautifulSoup(thepage, "html.parser")
     return soupdata
 
-#soup = make_soup("http://www.basketball-reference.com/players/a/")
-soup = make_soup("http://erail.in/?T=HPT::YPR")
+soup = make_soup("http://www.basketball-reference.com/players/a/")
+#soup = make_soup("http://erail.in/?T=HPT::YPR")
 
-for record in soup.findAll('td'):
+
+playerdatasaved =""
+for record in soup.findAll('tr'):
+    playerdata=""
     for data in record.findAll('td'):
-        for info in data.findAll('a'):
-            print info.text
+        playerdata = playerdata+","+data.text
+    playerdatasaved = playerdatasaved +"\n" + playerdata[1:]
 
-for r in soup.findAll('div', {"class":"BackColor2"}):
-        print r.find('a').text
+print playerdatasaved
+headed ="From,To,Pos,Ht,Wt,Birth Date,Birth Year,College"+"\n"
+file1 = open(os.path.expanduser("basketball.csv"),"wb")
+file1.write(bytes(headed))
+file1.write(bytes(playerdatasaved))
+##for r in soup.findAll('div', {"class":"BackColor2"}):
+##        print r.find('a').text
+##
